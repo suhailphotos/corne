d9c7a2dc384cd3a81f6aa53068c463339131a8e9
Suhail Suhail <suhailece@gmail.com>
2025-10-31 13:35:57 -0700

    qmk(kite): add UniCorne keymap (layers/macros/combos) + base config

---
+++ qmk/keyboards/boardsource/unicorne/keymaps/suhail/rules.mk
@@ -0,0 +1,9 @@
+# Core features
+COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+# Choose ONE lighting system later (commented for now)
+# RGBLIGHT_ENABLE = yes
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = WS2812
