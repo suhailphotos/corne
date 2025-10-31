83d6a7154dd46e255fa5f15e3e4903ae6240767e
Suhail Suhail <suhailece@gmail.com>
2025-10-31 15:08:56 -0700

    qmk(kite): import default boardsource/unicorne keymap as baseline

--- qmk/keyboards/boardsource/unicorne/keymaps/suhail/rules.mk
+++
@@ -1,9 +0,0 @@
-# Core features
-COMBO_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-
-# Choose ONE lighting system later (commented for now)
-# RGBLIGHT_ENABLE = yes
-# RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
